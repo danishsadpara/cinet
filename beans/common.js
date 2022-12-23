21a7aaed1f532b59a65aafa057ba0c77d22d6207
danishsadpara <danishsadpara@gmail.com>
2022-12-23 14:40:02 +0500

    validations of admin and client along with encruption is added

--- beans/common.js
+++ beans/common.js
@@ -25,29 +25,51 @@ const singup = async (body) => {
 
         switch (userType) {
             case 'admin':
-                //homewrok -apply admin fields validation here
+                // homewrok -apply admin fields validation here
+                if (!body.data.firstName) {
+                    return Promise.reject({ error: "firstName is required" });
+                }
+                if (!body.data.lastName) {
+                    return Promise.reject({ error: "lastName is required" });
+                }
+                if (!body.data.dob) {
+                    return Promise.reject({ error: "date of birth is required" });
+                }
+
                 result = await adminController.addAdmin(body.data);
                 break;
             case 'client':
-                //homewrok -apply admin fields validation here
+                //homewrok -apply client fields validation here
+                if (!body.data.firstName) {
+                    return Promise.reject({ error: "firstName is required" });
+                }
+                if (!body.data.lastName) {
+                    return Promise.reject({ error: "lastName is required" });
+                }
+                if (!body.data.DOB) {
+                    return Promise.reject({ error: "date of birth  is required" });
+                }
+                if (!body.data.age) {
+                    return Promise.reject({ error: "age is required" });
+                }
                 result = await clientController.addClient(body.data);
                 break;
             default:
                 return Promise.reject({ error: "userType is invalid" });
-            
+
         }
-// public to authentication attached .
-   const userJson={
-    userName: body.userName,
-    password:body.password, // make this password ecrypted.
-    userType:{
-        kind: userType,
-        item: result._id //now user is dynamic.
-    }
-   };
-   const user = await usersController.addUser(userJson);
-   return user;
-//    email can also send from here.
+        // public to authentication attached .
+        const userJson = {
+            userName: body.userName,
+            password: body.password, // make this password ecrypted.
+            userType: {
+                kind: userType,
+                item: result._id //now user is dynamic.
+            }
+        };
+        const user = await usersController.addUser(userJson);
+        return user;
+        //    email can also send from here.
     } catch (ex) {
         return Promise.reject({ error: ex });
     }
--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose'); //hardcoded
+const bcrypt=require('bcryptjs');
 const usersSchema = new mongoose.Schema({
     userName: {
         type: String,
@@ -18,4 +19,12 @@ const usersSchema = new mongoose.Schema({
         collection: 'users'  //model ka synonym
     }
 ); //schema definition -it creeates mongose schema-we create json keys
+
+// password enryption
+usersSchema.pre('save',async function(next){
+    if(!this.isModified('password')){
+        next()
+    }
+    this.password=await bcrypt.hash(this.password, 10)
+});
 module.exports = mongoose.model('users', usersSchema); //map models to schema.
